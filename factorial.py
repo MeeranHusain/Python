6e47daf844afb5bcbe28b358c367ff76e6a4e130
Meeran Husain <meeranhusain3@gmail.com>
2024-10-27 16:59:12 +0530

    python practice

---
+++ factorial.py
@@ -0,0 +1,11 @@
+# Write a program for the factorial 
+fact = 1
+# num = (int(input("Enter the number: ")))
+# for i in range(1,num+1):
+#     fact = fact * i
+#     print(fact)
+
+
+for i in range(4,1,-1):
+    fact = fact*i
+    print(fact)
