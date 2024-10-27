6085c07be7bf16920ea8f78e750bffa90ecc628d
Meeran Husain <meeranhusain3@gmail.com>
2024-10-27 17:05:47 +0530

    python practice

---
+++ python/largestNumber.py
@@ -0,0 +1,10 @@
+# WAP to print which is the largest number using nested if-else
+a = 10
+b = 20
+c = 5
+if (a>b) and (a>c):
+    print(a,'A is greater')
+elif (b>c):
+    print(b,'B is greater')
+else:
+    print(c,'C is greater')
---
+++ python/main.py
@@ -0,0 +1,7 @@
+# Write a program to print even and odd number between(81 to 252)
+for num in range(81,252):
+    if num%2==0:
+       print(num,"is even")
+    else:
+        print(num,"is odd")
+         
