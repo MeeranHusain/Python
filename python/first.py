1242c11bbb8f51e8ab012440dab9269b663ce653
Meeran Husain <meeranhusain3@gmail.com>
2024-10-28 22:00:44 +0530

    python practice notes

---
+++ python/first.py
@@ -0,0 +1,49 @@
+# ------------- All Python notes -----------------
+
+# --- Data types in python ---
+# int ----> integer is number
+# str ----> string   "hello"   'hello'      ''' hello '''   """ hello """
+# float ----> float is decimal number e.g:- 98.99000
+# list ----> list is like array / [] list() 
+# tuple ----> (1,) tuple ()
+# dict ----> dictionary {} / {'key': 'value'}
+# set ----> set {'value'}
+# None ----> 
+# bool ----> True or False value
+# Complex ----> a = 12+2j ----> the j is for the imaginary part
+
+# Everything in the pyhton is object 
+
+# 0b ----> binary
+# 0x ----> hexadecimal
+# 0o ----> octal
+# 0xf ----> 25  
+
+# --- Typecasting --- (by default it was str you have to convert into integar or in float)
+# type(int(input("Hello world")))
+# Output is :- int 
+
+# Python only supports single line comment it is represented by Hash sign or Pound sign.
+
+# Python block are indetation 
+
+# everything in the python is object
+
+# Mutable and immutable  
+# string, numbers and tuples are immutable
+
+# String is immutable because, Strings in Python are “immutable” which means they can not be changed after they are created. Some other immutable data types are integers, float, boolean, etc.
+
+# list, Dictionary, set is not hashable type eg; hash[1] ---> this throw an error 
+# hashable are --> string, numbers, tuple, complex.
+# set and dictionary used hash.
+# hash is a function that converts larger files and stores in the database.
+# set is mutable and dictionary is mutable.
+
+# function is need to 
+# method needs object 
+# methods works on a object data
+
+# dictionary key should be unique and hashable
+
+#  
