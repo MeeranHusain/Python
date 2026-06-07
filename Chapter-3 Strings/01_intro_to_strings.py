d78b9410c17072e58388698a4be2d772caccaaf6
Meeran Husain <meeranhusain3@gmail.com>
2026-06-07 23:05:38 +0530

    chapter 3 string topic

---
+++ Chapter-3 Strings/01_intro_to_strings.py
@@ -0,0 +1,7 @@
+a ='Meeran'       # Single quoted string 
+b = "Meeran"      # Double quoted string 
+c = '''Meeran'''  # Triple quoted string
+
+# String is immutable, because once string is created it can't be change that string, if you want to change the particular character of a string you cannot change the string. you have to create another string.
+
+
