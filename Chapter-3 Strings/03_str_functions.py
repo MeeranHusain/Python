661d280336a75b3dbaaccf3842d5808b3c50962d
Meeran Husain <meeranhusain3@gmail.com>
2026-06-10 22:51:54 +0530

    string functions and its use

--- Chapter-3 Strings/03_str_functions.py
+++ Chapter-3 Strings/03_str_functions.py
@@ -1,2 +1,8 @@
-name = "Meeran"
+name1 = "meeRan "
+print(name1.capitalize())
+
+name = "Meeran husain"
 print(len(name))
+print(name.endswith("in"))
+print(name.startswith("an"))
+print(name.replace("husain", "shaikh"))
