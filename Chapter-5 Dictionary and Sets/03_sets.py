2b98677ae2a89695a893b1d45cbf2cdc29161100
Meeran Husain <meeranhusain3@gmail.com>
2026-06-19 22:09:53 +0530

    Chap-5 Intro of Dictionary, Sets and its methods

---
+++ Chapter-5 Dictionary and Sets/03_sets.py
@@ -0,0 +1,19 @@
+# Set is a collection of non-repetitive elements.
+
+# s = {}      # this is the empty dictionary.
+
+# s = {1, 343, 64}    # This is the set.
+# print(type(s))
+
+# emptySet = set()    # Now this become empty set.
+# print(type(s))
+
+s = {1, 343, 64, 5, 5, 5}    # In Set the value will not repeat and this will not mantain the order, if you want to maintain the order then use List.
+print(s, type(s))  
+
+
+# PROPERTIES OF SETS 
+# 1. Sets are unordered => Element’s order doesn’t matter 
+# 2. Sets are unindexed => Cannot access elements by index 
+# 3. There is no way to change items in sets. 
+# 4. Sets cannot contain duplicate values.
---
+++ Chapter-5 Dictionary and Sets/04_set_methods.py
@@ -0,0 +1,8 @@
+s = {1, 343, 64, 5, 5, 5, "Meeran"}    # In Set the value will not repeat and this will not mantain the order, if you want to maintain the order then use List.
+
+s.add(566)
+print(s, type(s))
+s.remove(1)
+print(s, type(s))
+
+# There are more methods of sets refer to the python.org or W3School etc, websites
