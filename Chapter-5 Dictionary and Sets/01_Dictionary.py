7b11d1d9c2cc72b993ed01699b0f81c60f7627fa
Meeran Husain <meeranhusain3@gmail.com>
2026-06-19 19:16:39 +0530

    Introduction to the Dictionary in Python

---
+++ Chapter-5 Dictionary and Sets/01_Dictionary.py
@@ -0,0 +1,20 @@
+# Dictionary is a collection of keys-value pairs.
+marks = {
+    "Meeran"    : 100,
+    "Pankaj"    : 90,
+    "Faiz"      : 80,
+    "Rohan"     : 60,
+    "List"      : [1,2,3]
+}
+
+print(marks, type(marks))
+print(marks["Meeran"])
+print(marks["List"], type(marks["List"]))
+
+''' 
+PROPERTIES OF PYTHON DICTIONARIES
+ 1. It is unordered.
+ 2. It is mutable. 
+ 3. It is indexed. 
+ 4. Cannot contain duplicate keys
+'''
