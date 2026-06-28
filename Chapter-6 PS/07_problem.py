52c387705afceb1c7faeb2ff4733aae97afccbd2
Meeran Husain <meeranhusain3@gmail.com>
2026-06-28 21:23:24 +0530

    Chap-6 intro of conditional statement and its Practice Set

---
+++ Chapter-6 PS/07_problem.py
@@ -0,0 +1,11 @@
+# Write a program to find out whether a given post is talking about “Harry” or not.
+
+# post = "Hey harry bhai is good Harry is very good and harry is great"
+post = input("Enter the post: ")
+
+if("Harry" in post):
+    print("This post is talking about harry")
+    
+else:
+    print("This post is not talking about harry")
+    
