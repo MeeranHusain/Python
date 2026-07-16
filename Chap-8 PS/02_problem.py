cf16da60a78a7c874bba28888b03e51516e5d792
Meeran Husain <meeranhusain3@gmail.com>
2026-07-16 22:25:35 +0530

    Chap-8 (Write a python program using function to convert Celsius to Fahrenheit.)

--- Chap-8 PS/02_problem.py
+++ Chap-8 PS/02_problem.py
@@ -4,4 +4,7 @@ def fahrenheit_to_celsius(f):
     return 5*(f-32)/9
     
 f = int(input("Enter the temperature in F: "))
-print(f"{fahrenheit_to_celsius(f):.2f} °C")
+c = fahrenheit_to_celsius(f)
+# print(f"{c:.2f} °C")
+print(f"{round(c, 2)} °C")
+
