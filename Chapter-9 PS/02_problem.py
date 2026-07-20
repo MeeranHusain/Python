f3de465e423d8af28a7cfac18f8e3e975e509634
Meeran Husain <meeranhusain3@gmail.com>
2026-07-20 23:09:10 +0530

    Chap-9 (hiscore game)

---
+++ Chapter-9 PS/02_problem.py
@@ -0,0 +1,26 @@
+# The game() function in a program lets a user play a game and returns the score as an integer. You need to read a file ‘Hi-score.txt’ which is either blank or  contains the previous Hi-score. You need to write a program to update the Hi-score whenever the game() function breaks the Hi-score. 
+
+import random as ra
+
+def game():
+    print("You are playing the game..")
+    score = ra.randint(1, 65)
+    
+    # fetch the hiscore
+    with open("Chapter-9 PS/hiscore.txt") as f:
+        hiscore = f.read()
+        if (hiscore != ""):
+            hiscore = int(hiscore)
+        else:
+            hiscore = 0
+            
+    print(f"Your score: {score}")
+    if (score>hiscore or hiscore == ""):
+        # write this hiscore to the file
+        with open("Chapter-9 PS/hiscore.txt", "w") as f:
+            f.write(str(score))         
+    
+    return score
+
+game()
+ 
