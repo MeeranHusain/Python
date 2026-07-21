43c4130fbcc9b7b69c47c7a8429c2258cba5455e
Meeran Husain <meeranhusain3@gmail.com>
2026-07-21 22:37:44 +0530

    Chap-9 (Write a program to generate multiplication tables from 2 to 20 and write it to the different files. Place these files in a folder for a 13 – year old. )

--- Chapter-9 PS/03_problem.py
+++ Chapter-9 PS/03_problem.py
@@ -5,7 +5,7 @@ def generateTable(n):
     for i in range (1, 11):
         table += f"{n} X {i} = {n*i}\n"
                 
-    with open(f"Chapter-9 PS/table/table_{n}", "w") as f:
+    with open(f"Chapter-9 PS/tables/table_{n}.txt", "w") as f:
         f.write(table)
 
 for i in range(2, 21):
