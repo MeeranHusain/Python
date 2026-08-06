5722b371085bfca2a6cf3a5000dbd94a70914384
Meeran Husain <meeranhusain3@gmail.com>
2026-08-06 21:52:58 +0530

    Chap-11 (Add Employee class with salary and increment properties)

---
+++ Chapter-11 PS/03_problem.py
@@ -0,0 +1,10 @@
+# Create a class ‘Employee’ and add salary and increment properties to it.
+
+
+class Employee:
+    salary = 234
+    increment = 20
+
+
+e = Employee()
+print(f"Salary: {e.salary} Increment: {e.increment}")
