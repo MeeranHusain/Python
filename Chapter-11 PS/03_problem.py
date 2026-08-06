67df9d2a9a72330a7c011cbf38a21f3dda44e7d3
Meeran Husain <meeranhusain3@gmail.com>
2026-08-06 23:16:16 +0530

    Chap-11 (Implement Employee class with salary and increment properties; add salaryAfterIncrement method with property decorator)

--- Chapter-11 PS/03_problem.py
+++ Chapter-11 PS/03_problem.py
@@ -1,10 +1,20 @@
-# Create a class ‘Employee’ and add salary and increment properties to it.
-
+# 3. Create a class ‘Employee’ and add salary and increment properties to it. Write a method ‘salaryAfterIncrement’ method with a @property decorator with a setter which changes the value of increment based on the salary. 
 
 class Employee:
     salary = 234
     increment = 20
+    
+    @property
+    def salaryAfterIncrement(self):
+        return self.salary + (self.salary * self.increment / 100)
+    
+    @salaryAfterIncrement.setter
+    def salaryAfterIncrement(self, salary):
+        # self.increment = (salary - self.salary) / self.salary * 100
+        self.increment = ((salary / self.salary) - 1) * 100
 
 
 e = Employee()
-print(f"Salary: {e.salary} Increment: {e.increment}")
+# print(e.salaryAfterIncrement)  # Output: 280.8"
+e.salaryAfterIncrement = 280.8
+print(e.increment)  
