7c8602df56bac93f012b842d948692b777a12113
Meeran Husain <meeranhusain3@gmail.com>
2026-08-14 18:12:31 +0530

    Chap-12 (Add example demonstrating the use of enumerate() function in Python)

---
+++ Chapter-12 Advance Python 1/10_enumerate.py
@@ -0,0 +1,12 @@
+l = [3, 5, 7, 9, 11]
+
+# index = 0
+# for item in l:
+#     print(f"The item at index {index} is: {item}")
+    # index += 1
+    
+# This can be done in a better way using enumerate() function
+
+for index, item in enumerate(l):
+    print(f"The item at index {index} is: {item}")
+     
