c59fdbd8096f3b4609f1243c1e22eb09a1c696c7
Meeran Husain <meeranhusain3@gmail.com>
2026-08-14 21:40:04 +0530

    Chap-13 (Add virtual environment setup instructions and requirements file)

---
+++ Chapter-13 Advanced Python 2/01_venv.py
@@ -0,0 +1,16 @@
+'''
+
+To use virtual environments, we write: 
+# pip install virtualenv  # Install the package 
+
+We create a new environment using:      
+# virtualenv myprojectenv # Creates a new venv 
+
+The next step after creating the virtual environment is to activate it. 
+We can now use this virtual environment as a separate Python installation. 
+
+# To activate virtual environment cmd: .\env\Scripts\activate.ps1    
+
+# To deactivate virtual environment cmd: deactivate
+
+'''
