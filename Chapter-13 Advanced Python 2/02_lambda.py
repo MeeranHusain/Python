4150d8af35056be09f7b564c6e093236d17c250e
Meeran Husain <meeranhusain3@gmail.com>
2026-08-16 22:50:11 +0530

    Chap-13 (Add lambda function example to demonstrate usage)

---
+++ Chapter-13 Advanced Python 2/02_lambda.py
@@ -0,0 +1,14 @@
+# def square(n):
+#     return n*n
+
+# Function created using an expression using ‘lambda’ keyword.
+# lambda arguments:expressions 
+# can be used as a normal function
+
+square = lambda x: x * x
+print(square(4))
+
+
+# square = lambda x:x*x 
+# square(6)  # returns 36 
+# sum = lambda a,b,c:a+b+c 
