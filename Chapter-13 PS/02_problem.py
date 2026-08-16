87200007892c28f10c208fa4a0c0be3f23d54e0d
Meeran Husain <meeranhusain3@gmail.com>
2026-08-16 23:39:05 +0530

    Chap-13 PS(2. Write a program to input name, marks and phone number of a student and format it using the format function like below.)

---
+++ Chapter-13 PS/02_problem.py
@@ -0,0 +1,9 @@
+# 2. Write a program to input name, marks and phone number of a student and format it using the format function like below.
+
+name = input("Enter the name of the student: ")
+marks = float(input("Enter the marks of the student: "))
+phone_number = input("Enter the phone number of the student: ")
+
+formatted_output = "Student Name: {}\nMarks: {:.2f}\nPhone Number: {}".format(name, marks, phone_number)
+
+print(formatted_output)
