a44aa99a9053dd3a480e9eb2b0c2b0dc271670e7
Meeran Husain <meeranhusain3@gmail.com>
2026-08-16 23:04:42 +0530

    Chapter - 13 (Add formatting examples using str.format())

---
+++ Chapter-13 Advanced Python 2/04_format.py
@@ -0,0 +1,10 @@
+a = "{} is a good {}".format("harry", "boy")   #1. 
+b = "{1} is a good {0}".format("harry", "boy")  #2. 
+
+print(a)
+# output for 1: 
+# harry is a good boy  
+ 
+print(b)
+# output for 2: 
+# boy is a good harry
